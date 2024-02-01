ac170e70ad701696485f3bd0e9f47a087a97dc84
Vivek <vs690799@gmail.com>
2024-02-01 13:37:09 +0530

    some changes

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>This is a customers Page</div>
+  )
+}
+
+export default Page     
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>This is a invoices Page</div>
+  )
+}
+
+export default Page     
